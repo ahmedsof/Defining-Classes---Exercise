c0a4ec83cab77609ebba587775d88cb20cd98576
Ahmed Softev <ahmed_v@abv.bg>
2020-10-12 15:19:40 +0300

    C#

---
+++ 03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using DefiningClasses;
+using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ").ToArray();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
