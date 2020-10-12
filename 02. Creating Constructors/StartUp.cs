edad11edc5624e435cdfd0be63e7cad06e0f438c
Ahmed Softev <ahmed_v@abv.bg>
2020-10-12 14:39:41 +0300

    C#

---
+++ 02. Creating Constructors/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using DefiningClasses;
+using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person persone2 = new Person(12);
+        }
+    }
+}
