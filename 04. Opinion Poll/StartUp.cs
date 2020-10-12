a4ebc1a2c7ff3b45234815998d386b81fe2179eb
Ahmed Softev <ahmed_v@abv.bg>
2020-10-13 00:38:47 +0300

    C#

---
+++ 04. Opinion Poll/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using DefiningClasses;
+using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ").ToArray();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            Person[] people = family.GetPeople();
+
+            foreach (var person in people)
+            {
+
+
+
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
